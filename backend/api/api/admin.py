65f3e92153739904c4d23450a3affd2b7cbbe985
mkoner <34752645+mkoner@users.noreply.github.com>
2023-09-25 21:04:45 +0000

    Delete backend directory

--- backend/api/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models.admin import Admin
-from .models.course import Course
-from .models.course_material import CourseMaterial
-from .models.learner import Learner
-
-admin.site.register([Admin, Course, Learner, CourseMaterial])
