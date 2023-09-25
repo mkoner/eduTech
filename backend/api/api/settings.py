c3a978c8ed87c3f651a24fd6e75137bccfc44f83
mkoner <amzid@hotmail.fr>
2023-09-25 14:25:58 +0000

    Switched to mysql and enabled CORS

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = 'django-insecure-pyb0qgk&&7=&s8q3n!gax=2c^2^mt-03)@@a2_!&+&s9@_^tz_
 DEBUG = True
 
 ALLOWED_HOSTS = ['edutech-36t5.onrender.com']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -80,20 +82,20 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-POST_DB_NAME = getenv('POST_DB_NAME', 'edutech')
-POST_DB_USER = getenv('POST_DB_USER', 'edutech_user')
-POST_DB_PASSWORD = getenv('POST_DB_PASSWORD', '@edutech_001')
-POST_DB_HOST = getenv('POST_DB_HOST', 'localhost')
-POST_DB_PORT = getenv('POST_DB_PORT', '5432')
+MYSQL_DB_NAME = getenv('MYSQL_DB_NAME', 'edutech')
+MYSQL_DB_USER = getenv('MYSQL_DB_USER', 'edutech_user')
+MYSQL_DB_PASSWORD = getenv('MYSQL_DB_PASSWORD', '@edutech_001')
+MYSQL_DB_HOST = getenv('MYSQL_DB_HOST', 'localhost')
+MYSQL_DB_PORT = getenv('MYSQL_DB_PORT', '3306')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': POST_DB_NAME,
-        'USER': POST_DB_USER,
-        'PASSWORD': POST_DB_PASSWORD,
-        'HOST': POST_DB_HOST,   
-        'PORT': POST_DB_PORT,
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': MYSQL_DB_NAME,
+        'USER': MYSQL_DB_USER,
+        'PASSWORD': MYSQL_DB_PASSWORD,
+        'HOST': MYSQL_DB_HOST,   
+        'PORT': MYSQL_DB_PORT,
     }
 }
 
